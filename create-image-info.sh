45e66208ebccb5349ff7479e451da833df184042
dackjanie <164849775+dackjanie@users.noreply.github.com>
2024-06-19 15:39:44 +0530

    Update create-image-info.sh

--- create-image-info.sh
+++ create-image-info.sh
@@ -2,14 +2,13 @@
 
 set -ex
 
-REPO=$(cat ${REPO_NAME}/repository)
-TAG=$(cat ${REPO_NAME}/tag)
+REPO=$(cat "${REPO_NAME}"/repository)
+TAG=$(cat "${REPO_NAME}"/tag)
 IMAGE_INFO="${REPO}:${TAG}"
-echo $IMAGE_INFO | tee image-info/image
+echo "$IMAGE_INFO" | tee image-info/image
 
 max=10
-for i in `seq 2 $max`
-do
+for i in $(seq 2 $max); do
     echo "$i"
 done
 
