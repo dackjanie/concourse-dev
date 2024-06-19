c7ba52ef57c08e2b8c50a328f65c452d163e5804
dackjanie <164849775+dackjanie@users.noreply.github.com>
2024-06-19 13:05:10 +0530

    Update create-image-info.sh

--- create-image-info.sh
+++ create-image-info.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-REPO=$(cat $(REPO_NAME)/repository)
-TAG=$(cat $(REPO_NAME)/tag)
+REPO=$(cat ${REPO_NAME}/repository)
+TAG=$(cat ${REPO_NAME}/tag)
 echo "${REPO}:${TAG}" > image-info/image
