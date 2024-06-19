dd2ff1309749d7674cbf197dbe52a66b3516503a
dackjanie <164849775+dackjanie@users.noreply.github.com>
2024-06-19 15:29:44 +0530

    Update create-image-info.sh

--- create-image-info.sh
+++ create-image-info.sh
@@ -4,4 +4,14 @@ set -ex
 
 REPO=$(cat ${REPO_NAME}/repository)
 TAG=$(cat ${REPO_NAME}/tag)
-echo "${REPO}:${TAG}" > image-info/image
+IMAGE_INFO="${REPO}:${TAG}"
+echo $IMAGE_INFO | tee image-info/image
+
+# Print numbers from 1 to 5
+for i in {1..5}
+do
+   echo "Number: $i"
+done
+
+# Print a simple message
+echo "Processing complete"
